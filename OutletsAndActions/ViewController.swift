ecb97d8b6d65f8a4c5d2402f913e4929d7d11db3
Alexander Sobolev <sobolev.ios.developer@gmail.com>
2020-12-15 17:10:11 +0300

    Added button and Label

--- OutletsAndActions/ViewController.swift
+++ OutletsAndActions/ViewController.swift
@@ -9,6 +9,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var Label: UILabel!
+    
+    
+    @IBOutlet var LabelCollection: [UILabel]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -16,7 +21,8 @@ class ViewController: UIViewController {
 
 
     @IBAction func buttonTapped(_ sender: UIButton) {
-        print("Hello Word!")
+        print("Hello Word")
+        Label.text = "Hello Label
     }
 }
 
