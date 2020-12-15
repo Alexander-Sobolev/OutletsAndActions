2c7abe6dd95d29cca7e27826d182398f7fc95a0d
Alexander Sobolev <sobolev.ios.developer@gmail.com>
2020-12-15 16:41:15 +0300

    Initial Commit

---
+++ OutletsAndActions/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  OutletsAndActions
+//
+//  Created by Alexander Sobolev on 15.12.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    @IBAction func buttonTapped(_ sender: UIButton) {
+    }
+}
+
